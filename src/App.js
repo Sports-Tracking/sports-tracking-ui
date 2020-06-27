ab2e58e69d8fc3da6da994ea017ff1b512c70a24
Farooq Khan <33574913+Farooq-azam-khan@users.noreply.github.com>
2020-06-27 17:34:16 -0400

    created landing page

--- src/App.js
+++ src/App.js
@@ -1,26 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import { Switch, Route, useParams } from "react-router-dom";
+import LandingPage from './components/LandingPage'
+const App = () => {
+  return (<Switch>
+    <Route exact path="/"><LandingPage /></Route>
+    <Route exact path="/soccer/:id"><SoccerLeague /></Route>
+  </Switch>)
 }
+
+const SoccerLeague = () => {
+  let { id } = useParams();
+  return <div>{id}</div>
+}
+
 
 export default App;
